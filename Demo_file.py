68a913471862ffe4b99e09906f301b6e56fc85c4
Monalij28 <125961619+Monalij28@users.noreply.github.com>
2024-08-11 22:37:27 +0530

    Adding_python_file
    Arithmatic operation

---
+++ Demo_file.py
@@ -0,0 +1,14 @@
+x=int(input("enter value for x"))
+y=int(input("Enter value for y"))
+
+
+def Addition(a,b):
+    return a+b
+
+
+def Subtraction(a,b):
+    return a-b
+
+
+print(Addition(x,y))
+print(Subtraction(x,y))
